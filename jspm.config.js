0b53e35f649f462fe8b04fd74b2e29d16400debc
Karolusrex <kalle.lundfall@gmail.com>
2017-11-23 10:11:58 +0100

    Made a working example in the new arva

--- jspm.config.js
+++ jspm.config.js
@@ -2,29 +2,12 @@ SystemJS.config({
   babelOptions: {
     'plugins': [
       'babel-plugin-transform-decorators-legacy',
-      'babel-plugin-transform-class-properties',
-      [
-        'babel-plugin-transform-builtin-extend',
-        {
-          'globals': [
-            'Array'
-          ],
-          'approximate': true
-        }
-      ],
-      [
-        'babel-plugin-transform-es2015-spread',
-        {
-          'loose': true
-        }
-      ],
-      [
-        'babel-plugin-transform-es2015-for-of',
-        {
-          'loose': true
-        }
-      ]
-    ]
+      'babel-plugin-transform-class-properties'
+    ],
+    'es2015': false,
+    'stage3': true,
+    'stage2': true,
+    'stage1': false
   },
   arvaOptions: {
     'iconOptions': {
--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,10 +1,21 @@
-import Surface              from 'famous/core/Surface.js';
-import {View}               from 'arva-js/core/View.js';
-import {layout, event}      from 'arva-js/layout/Decorators.js';
+import Surface                  from 'famous/core/Surface.js';
+import {View}                   from 'arva-js/core/View.js';
+import {layout,
+    event,
+    bindings, flow}             from  'arva-js/layout/Decorators.js';
+
+@bindings.setup({
+    welcomeName: 'noName'
+})
 
 export class HomeView extends View {
-    @layout.size(~100, ~25)
-    @layout.stick.center()
-    message = new Surface({content: `Hello ${this.options.welcomeName}`});
+    @event.on('click', function () {
+        this.options.welcomeName = 'you clicked me...';
+    })
+    @flow.auto()
+    @layout
+        .size(~100, ~25)
+        .stick.center()
+    message = new Surface({content: `Hello ${this.options.welcomeName}`, properties: {textAlign: 'center'}});
 
 }
